9463d4ab65e391eaa04e799ed88d34d9d9c1a46c
dedi45458 <dedi45458@gmail.com>
2026-06-11 05:06:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -439,8 +439,9 @@ if st.session_state['proses_selesai']:
         
         # Kolom sesuai permintaan: Tanpa Baris Excel, Tipe Sasaran -> Validasi -> Justifikasi
         kolom_susunan = [
-            "Pilih", "Lembaga SSR", "Tanggal", "ID Klien", "INDIKATOR KESALAHAN DATA", 
-            "Kode Petugas", "Nama Kota", "NIK", "Tipe Sasaran", "validasi hasil review", "Justifikasi"
+            "Pilih", "Lembaga SSR", "Tanggal", "ID Klien", 
+            "Kode Petugas", "Nama Kota", "NIK", "Tipe Sasaran", 
+            "IIndikator Kesalahan Data", "Validasi hasil review", "Justifikasi"
         ]
         
         df_bawah_view = st.session_state['df_tabel_bawah'][kolom_susunan].copy()
