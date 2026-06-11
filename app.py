d3a96cb23450209a0a534c9d517132fe67f4d99d
dedi45458 <dedi45458@gmail.com>
2026-06-12 02:14:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -180,9 +180,9 @@ ATURAN_VALIDASI_BAWAAN = [
     # TAMBAHAN BARU: Validasi jumlah batas wajar Logistik
     {"nama": "Konfirmasi jumlah KIE yang diberikan adalah wajar (konfirmasi)", "periksa": lambda c: c['log_kie'] > 5},
     {"nama": "Konfirmasi jumlah kondom yang diberikan adalah wajar (konfirmasi)", "periksa": lambda c: c['log_kon'] > 144},
-    {"nama": "Konfirmasi jumlah pelicin yang diberikan adalah wajar (konfirmasi)", "periksa": lambda c: c['log_pel'] > 50},
-    {"nama": "Konfirmasi jumlah jarum yang diberikan adalah wajar (konfirmasi)", "periksa": lambda c: c['log_jar'] > 10},
-    {"nama": "Konfirmasi jumlah alkohol SWAB yang diberikan adalah wajar (konfirmasi)", "periksa": lambda c: c['log_swab'] > 50},
+    {"nama": "Konfirmasi jumlah pelicin yang diberikan adalah wajar (konfirmasi)", "periksa": lambda c: c['log_pel'] > 100},
+    {"nama": "Konfirmasi jumlah jarum yang diberikan adalah wajar (konfirmasi)", "periksa": lambda c: c['log_jar'] > 30},
+    {"nama": "Konfirmasi jumlah alkohol SWAB yang diberikan adalah wajar (konfirmasi)", "periksa": lambda c: c['log_swab'] > 30},
     
     # PERBAIKAN NAMA RULE: Sesuai deskripsi harusnya diisi 2 (Tatap Muka)
     {"nama": "VO tapi kolom Virtual dan Tatap Muka (VC1) diisi angka 2", "periksa": lambda c: c['is_vo'] and c['vc1'] == '2'},
