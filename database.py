1e72cf4b30205a2a0bd87c2604bfd9a6ffc4e34b
dedi45458 <dedi45458@gmail.com>
2026-06-17 13:29:43 +0700

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import pandas as pd
 from sqlalchemy import create_engine
-from datetime import datetime
+import datetime
 
 def dapatkan_koneksi_neon():
     """Membuka koneksi aman ke Neon Postgres menggunakan connection pooling."""
