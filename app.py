a267d3035053dd921081ebaa2bbdaa4e4547319f
dedi45458 <dedi45458@gmail.com>
2026-06-19 20:55:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1072,7 +1072,7 @@ if menu_pilihan == "🎯 Dashboard Review Data":
             mask_rujukan = df_semua_error['INDIKATOR KESALAHAN DATA'].isin(ind_rujukan)
             
             # Eliminasi duplikasi baris fisik agar akurasi dihitung per entri data (bukan per jenis error)
-            df_err_penj_unik = df_semua_error[~mask_rujukan].drop_duplicates(subset=["Lembaga SSR", "Tanggal", "ID Klien"])
+            df_err_penj_unik = df_semua_error[~mask_rujukan].drop_duplicates(subset=["LEMBAGA SSR", "TANGGAL", "ID KKLIEN"])
             df_err_ruj_unik = df_semua_error[mask_rujukan].drop_duplicates(subset=["Lembaga SSR", "Tanggal", "ID Klien"])
             
             tot_err_penj = len(df_err_penj_unik)
