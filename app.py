82843b492c22f24337ed68ec9f324561ea57e1cf
dedi45458 <dedi45458@gmail.com>
2026-06-21 14:50:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from zoneinfo import ZoneInfo
 # ==========================================================
 # IMPORT FUNGSI NEON DARI FILE database.py
 # ==========================================================
-From database import (
+from database import (
     dapatkan_koneksi_neon,
     simpan_log_ke_neon,
     jalankan_agregasi_tren,
